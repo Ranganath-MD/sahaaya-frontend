1ae8fe21241f97ba3e1225c0aa203d003396c8f3
Ranganath-MD <>
2021-05-23 11:27:06 +0530

    resolved firebase error

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ export const config = {
   local: "http://localhost:8080",
   prod: "https://dev-sahaaya.herokuapp.com",
   wslocal: "ws://localhost:8080",
-  wsProd: "ws://dev-sahaaya.herokuapp.com"
+  wsProd: "wss://dev-sahaaya.herokuapp.com"
 };
