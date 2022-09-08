8639e78415596a98af1970287a75154cc959b69f
Ranganath MD <ranganath.developer@gmail.com>
2022-09-08 05:25:11 +0000

    switched to render

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   local: "http://localhost:8080",
-  prod: "https://sahaaya-backend.herokuapp.com/",
+  prod: "https://sahaaya-backend.onrender.com/",
   wslocal: "ws://localhost:8080",
-  wsProd: "wss://sahaaya-backend.herokuapp.com",
+  wsProd: "wss://sahaaya-backend.onrender.com",
 };
